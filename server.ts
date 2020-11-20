34f516fbea817f2f215b186612658690fe092e4f
Justin Back <jback@pixelcatproductions.net>
2020-11-20 14:13:51 +0100

    Print external server ip to console

--- server.ts
+++ server.ts
@@ -15,6 +15,7 @@ import * as url from 'url';
 import * as JSON5 from 'json5';
 import * as Package from './package.json';
 import * as crypto from 'crypto';
+import * as publicIp from 'public-ip';
 function objToString(obj) {
 	var str = '';
 	for (var p in obj) {
@@ -39,145 +40,149 @@ console.log(color.red(`BETA Project by Justin Back`));
 console.log(color.blue("********************** Initializing ************************"));
 
 export const regexs = loadRegex();
-
-
-const httpserver = http.createServer((req, res) => {
-	res.writeHead(200, { 'content-type': 'text/html' })
-	fs.createReadStream('docs/index.html').pipe(res)
-})
+publicIp.v4().then((ip) => {
 
 
 
-const app = express();
-let dotenvparsed = dotenv.config();
-console.log(color.green("Loaded environment variables!"));
-dotenvparsed.parsed.API_KEY = "***";
-console.log(color.cyan(objToString(dotenvparsed.parsed)));
+	const httpserver = http.createServer((req, res) => {
+		res.writeHead(200, { 'content-type': 'text/html' })
+		fs.createReadStream('docs/index.html').pipe(res)
+	})
 
 
-// initialize a simple http server
-const server = http.createServer(app);
 
-// initialize the WebSocket server instance
-const wss = new WebSocket.Server({ server });
-console.log(color.blue("************ Starting Gateway Server ***************"));
-
-server.listen(parseInt(process.env.GATEWAY_PORT), '0.0.0.0', () => {
-	console.log(color.green(`Gateway server started on port ${color.cyan(process.env.GATEWAY_PORT)}`));
-	console.log(color.blue("********************** Done ************************"));
-	console.log(color.green("Gateway Server is now ready to accept connections"));
-});
-
-console.log(color.blue("************** Starting HTTP Server ****************"));
-httpserver.listen(parseInt(process.env.HTTP_PORT), "0.0.0.0", () => {
-	console.log(color.green(`HTTP Web server started on port ${color.cyan(process.env.HTTP_PORT)}`));
-	console.log(color.blue("********************** Done ************************"));
-	console.log(color.green("HTTP Server is now ready to accept connections"));
-});
-
-wss.on('connection', async (ws: any, req: any) => {
+	const app = express();
+	let dotenvparsed = dotenv.config();
+	console.log(color.green("Loaded environment variables!"));
+	dotenvparsed.parsed.API_KEY = "***";
+	console.log(color.cyan(objToString(dotenvparsed.parsed)));
 
 
-	ws.send = (function (_super) {
-		return function () {
-			// Extend it to log the value for example that is passed
-			if (parseInt(process.env.VERBOSITY) >= 2) {
-				console.log(color.cyan("[Outgoing Message]"), color.magenta(`<${ws.sessionID}>`), color.yellow(arguments[0]));
-			}
-			return _super.apply(this, arguments);
-		};
+	// initialize a simple http server
+	const server = http.createServer(app);
 
-	})(ws.send);
+	// initialize the WebSocket server instance
+	const wss = new WebSocket.Server({ server });
+	console.log(color.blue("************ Starting Gateway Server ***************"));
 
-	let ipAddress = req.socket.remoteAddress;
-	if (ipAddress === null || typeof ipAddress === "undefined" || ipAddress === "127.0.0.1") {
-		if (req.headers['x-forwarded-for']) {
-			ipAddress = req.headers['x-forwarded-for'].split(/\s*,\s*/)[0];
-		}
-		console.log(req.headers['x-forwarded-for']);
-	}
-
-
-	ws.ipAddress = ipAddress;
-	ws.sessionID = uuidv4();
-
-
-	if (parseInt(process.env.VERBOSITY) >= 2) {
-		console.log(color.green("[Session Created]"), color.magenta(`<${ws.sessionID}>`), color.green(ws.sessionID));
-	}
-
-	if (parseInt(process.env.VERBOSITY) >= 1) {
-		console.log(color.green("[Connection Open]"), color.magenta(`<${ws.sessionID}>`), color.green(req.url));
-	}
-	ws.on('close', async (code: any, reason: any) => {
-		//commands.get("close_connection")!.execute(ws, reason, wss);
-
-		if (parseInt(process.env.VERBOSITY) >= 1) {
-			console.log(color.yellow("[Connection Close]"), color.magenta(`<${ws.sessionID}>`), color.red(code), color.green(reason));
-		}
+	server.listen(parseInt(process.env.GATEWAY_PORT), process.env.GATEWAY_BIND, () => {
+		console.log(color.green(`Gateway server started on port ${color.cyan(process.env.GATEWAY_BIND)}:${color.cyan(process.env.GATEWAY_PORT)}`));
+		console.log(color.blue("********************** Done ************************"));
+		console.log(color.green("Gateway Server is now ready to accept connections"));
 	});
 
-	ws.on('error', async (err: any) => {
-		//commands.get("close_connection")!.execute(ws, err, wss);
-
-		if (parseInt(process.env.VERBOSITY) >= 1) {
-			console.log(color.red("[Connection Error]"), color.magenta(`<${ipAddress}>`), color.yellow(err));
-		}
+	console.log(color.blue("************** Starting HTTP Server ****************"));
+	httpserver.listen(parseInt(process.env.HTTP_PORT), process.env.HTTP_BIND, () => {
+		console.log(color.green(`HTTP Web server started on port ${color.cyan(process.env.HTTP_BIND)}:${color.cyan(process.env.HTTP_PORT)}`));
+		console.log(color.blue("********************** Done ************************"));
+		console.log(color.green("HTTP Server is now ready to accept connections"));
 	});
 
-	let Instance = req.url;
-	let Query = url.parse(Instance, true).query;
 
-
-
-	if (("Content-Type" in Query)) {
-		switch (Query["Content-Type"]) {
-			case "json":
-				ws.ContentType = 0;
-				break;
-			case "json5":
-				ws.ContentType = 1;
-				break;
-			default:
-				ws.ContentType = 0;
-		}
+	console.log(color.red("********************** INFOS ************************"));
+	if (ip == process.env.HTTP_BIND || process.env.HTTP_BIND == "0.0.0.0") {
+		console.log(color.green(`The webserver is reachable under http://${ip}:${process.env.HTTP_PORT}`));
 	} else {
-		ws.ContentType = 0;
+		console.log(color.green(`The webserver is reachable under http://${process.env.HTTP_BIND}:${process.env.HTTP_PORT}`));
 	}
+	if (ip == process.env.GATEWAY_BIND || process.env.GATEWAY_BIND == "0.0.0.0") {
+		console.log(color.green(`The gateway server is reachable under ws://${ip}:${process.env.GATEWAY_PORT}`));
+	} else {
+		console.log(color.green(`The gateway server is reachable under ws://${process.env.GATEWAY_BIND}:${process.env.GATEWAY_PORT}`));
+	}
+	console.log(color.blue("********************** Done ************************"));
 
-	ws.send(Response.message("session", { sessionID: ws.sessionID }, ws.ContentType));
+	wss.on('connection', async (ws: any, req: any) => {
 
 
+		ws.send = (function (_super) {
+			return function () {
+				// Extend it to log the value for example that is passed
+				if (parseInt(process.env.VERBOSITY) >= 2) {
+					console.log(color.cyan("[Outgoing Message]"), color.magenta(`<${ws.sessionID}>`), color.yellow(arguments[0]));
+				}
+				return _super.apply(this, arguments);
+			};
 
-	ws.on('message', async (message: string) => {
-		if (parseInt(process.env.VERBOSITY) >= 2) {
-			console.log(color.cyan("[Incoming Message]"), color.magenta(`<${ws.sessionID}>`), color.yellow(message));
+		})(ws.send);
+
+		let ipAddress = req.socket.remoteAddress;
+		if (ipAddress === null || typeof ipAddress === "undefined" || ipAddress === "127.0.0.1") {
+			if (req.headers['x-forwarded-for']) {
+				ipAddress = req.headers['x-forwarded-for'].split(/\s*,\s*/)[0];
+			}
+			console.log(req.headers['x-forwarded-for']);
 		}
 
 
+		ws.ipAddress = ipAddress;
+		ws.sessionID = uuidv4();
 
-		if (ws.ContentType === 0) {
-			if (!isJson(message)) {
-				ws.send(Response.error("invalid_json", 0, ws.ContentType));
-				return false;
+
+		if (parseInt(process.env.VERBOSITY) >= 2) {
+			console.log(color.green("[Session Created]"), color.magenta(`<${ws.sessionID}>`), color.green(ws.sessionID));
+		}
+
+		if (parseInt(process.env.VERBOSITY) >= 1) {
+			console.log(color.green("[Connection Open]"), color.magenta(`<${ws.sessionID}>`), color.green(req.url));
+		}
+		ws.on('close', async (code: any, reason: any) => {
+			//commands.get("close_connection")!.execute(ws, reason, wss);
+
+			if (parseInt(process.env.VERBOSITY) >= 1) {
+				console.log(color.yellow("[Connection Close]"), color.magenta(`<${ws.sessionID}>`), color.red(code), color.green(reason));
 			}
-		} else if (ws.ContentType === 1) {
-			if (!isJson5(message)) {
-				ws.send(Response.error("invalid_json5", 0, ws.ContentType));
-				return false;
+		});
+
+		ws.on('error', async (err: any) => {
+			//commands.get("close_connection")!.execute(ws, err, wss);
+
+			if (parseInt(process.env.VERBOSITY) >= 1) {
+				console.log(color.red("[Connection Error]"), color.magenta(`<${ipAddress}>`), color.yellow(err));
+			}
+		});
+
+		let Instance = req.url;
+		let Query = url.parse(Instance, true).query;
+
+
+
+		if (("Content-Type" in Query)) {
+			switch (Query["Content-Type"]) {
+				case "json":
+					ws.ContentType = 0;
+					break;
+				case "json5":
+					ws.ContentType = 1;
+					break;
+				default:
+					ws.ContentType = 0;
 			}
 		} else {
-			ws.send(Response.error("invalid_content_type", 0));
-			return false;
+			ws.ContentType = 0;
 		}
 
-		try {
+		ws.send(Response.message("session", { sessionID: ws.sessionID }, ws.ContentType));
 
-			let messageJSON;
+
+
+		ws.on('message', async (message: string) => {
+			if (parseInt(process.env.VERBOSITY) >= 2) {
+				console.log(color.cyan("[Incoming Message]"), color.magenta(`<${ws.sessionID}>`), color.yellow(message));
+			}
+
+
+
 			if (ws.ContentType === 0) {
-				messageJSON = JSON.parse(message);
+				if (!isJson(message)) {
+					ws.send(Response.error("invalid_json", 0, ws.ContentType));
+					return false;
+				}
 			} else if (ws.ContentType === 1) {
-				messageJSON = JSON5.parse(message);
+				if (!isJson5(message)) {
+					ws.send(Response.error("invalid_json5", 0, ws.ContentType));
+					return false;
+				}
 			} else {
 				ws.send(Response.error("invalid_content_type", 0));
 				return false;
@@ -185,101 +190,114 @@ wss.on('connection', async (ws: any, req: any) => {
 
 			try {
 
-				if (!("api_key" in messageJSON)) {
-					ws.send(Response.error("missing_parameter_api", 3, ws.ContentType));
-					return;
-				}
-				if (!("service" in messageJSON)) {
-					ws.send(Response.error("missing_parameter_service", 3, ws.ContentType));
-					return;
-				}
-				if (process.env.API_KEY !== messageJSON.api_key) {
-					ws.send(Response.error("api_key_mismatch", 0, ws.ContentType));
-					return;
+				let messageJSON;
+				if (ws.ContentType === 0) {
+					messageJSON = JSON.parse(message);
+				} else if (ws.ContentType === 1) {
+					messageJSON = JSON5.parse(message);
+				} else {
+					ws.send(Response.error("invalid_content_type", 0));
+					return false;
 				}
 
+				try {
+
+					if (!("api_key" in messageJSON)) {
+						ws.send(Response.error("missing_parameter_api", 3, ws.ContentType));
+						return;
+					}
+					if (!("service" in messageJSON)) {
+						ws.send(Response.error("missing_parameter_service", 3, ws.ContentType));
+						return;
+					}
+					if (process.env.API_KEY !== messageJSON.api_key) {
+						ws.send(Response.error("api_key_mismatch", 0, ws.ContentType));
+						return;
+					}
+
+				} catch (error) {
+					ws.send(Response.error("error_exec", 0, ws.ContentType));
+					if (parseInt(process.env.VERBOSITY) >= 3) {
+						console.log(color.red("[ERROR]", color.magenta("<JSON Exception Stacktrace>"), color.yellow(error.stack)));
+					} else {
+						console.log(color.red("[ERROR]", color.magenta("<JSON Exception>"), color.yellow(error)));
+					}
+				}
+				try {
+
+					console.log(color.magenta("[DocBot]"), color.magenta(`<${ws.sessionID}>`), color.green("Received Crawl Request for Service"), color.red(messageJSON.service));
+
+
+					const options = {
+						hostname: 'edit.tosdr.org',
+						port: 443,
+						path: '/api/v1/services/' + messageJSON.service,
+						method: 'GET'
+					}
+					let str = "";
+					let matches = [];
+					console.log(color.magenta("[DocBot]"), color.magenta(`<${ws.sessionID}>`), color.green("Starting Request to Service"), color.magenta(messageJSON.service), color.red(crypto.createHash('md5').update(messageJSON.service).digest("hex")));
+
+					const req = https.request(options, response => {
+						response.on('data', function (chunk) {
+							if (parseInt(process.env.VERBOSITY) >= 3) {
+								console.log(color.magenta("[DocBot]"), color.magenta(`<${ws.sessionID}>`), color.green("Received Phoenix Chunk"), color.red(crypto.createHash('md5').update(chunk).digest("hex")));
+							}
+							str += chunk;
+						});
+
+						//the whole response has been received, so we just print it out here
+						response.on('end', () => {
+							console.log(color.magenta("[DocBot]"), color.magenta(`<${ws.sessionID}>`), color.cyan("Digested Phoenix Chunk(s)"), color.red(crypto.createHash('md5').update(str).digest("hex")));
+							let parsed = JSON.parse(str);
+							for (var documentIndex in parsed.documents) {
+								console.log(color.magenta("[DocBot]"), color.magenta(`<${ws.sessionID}>`), color.green("Parsed Document"), color.green(parsed.documents[documentIndex].name), color.red(crypto.createHash('md5').update(parsed.documents[documentIndex].text).digest("hex")));
+
+								if (parsed.documents[documentIndex].text === null) {
+									console.log(color.magenta("[DocBot]"), color.magenta(`<${ws.sessionID}>`), color.yellow("Skipping document, it's empty!"), color.red(crypto.createHash('md5').update(parsed.documents[documentIndex].text).digest("hex")));
+									continue;
+								}
+								let Sentences = parsed.documents[documentIndex].text.split(".\n");
+								console.log(color.magenta("[DocBot]"), color.magenta(`<${ws.sessionID}>`), color.cyan("Parsing Sentences"), color.red(crypto.createHash('md5').update(parsed.documents[documentIndex].text).digest("hex")));
+
+
+								regexs.forEach((RegularExpression) => {
+									for (var index in Sentences) {
+										if (RegularExpression.expression.test(Sentences[index]) && (!matches.includes(RegularExpression.caseID) && process.env.NO_DUPLICATES == "1")) {
+
+											let quoteStart = parsed.documents[documentIndex].text.indexOf(Sentences[index]);
+											let quoteEnd = quoteStart + Sentences[index].length;
+
+											console.log(color.magenta("[DocBot]"), color.magenta(`<${ws.sessionID}>`), color.green("I have found a match on Line"), color.magenta(index), color.green("for the case"), color.magenta(RegularExpression.caseID), color.red(crypto.createHash('md5').update(Sentences[index]).digest("hex")));
+
+
+											ws.send(Response.match(striptags(Sentences[index]).replace(/\n/g, ''), RegularExpression.caseID, parsed.documents[documentIndex].id, quoteStart, quoteEnd, ws.ContentType))
+											matches.push(RegularExpression.caseID);
+										}
+									}
+								});
+							}
+						});
+					})
+
+					req.on('error', error => {
+						console.error(error)
+					})
+
+					req.end();
+				} catch (error) {
+					ws.send(Response.error("error_exec", 0, ws.ContentType));
+					console.log(color.red("[ERROR]", color.magenta(`<${messageJSON.service}>`), color.yellow(error)));
+				}
 			} catch (error) {
 				ws.send(Response.error("error_exec", 0, ws.ContentType));
 				if (parseInt(process.env.VERBOSITY) >= 3) {
-					console.log(color.red("[ERROR]", color.magenta("<JSON Exception Stacktrace>"), color.yellow(error.stack)));
+					console.log(color.red("[ERROR]", color.magenta("<Global Exception Stacktrace>"), color.yellow(error.stack)));
 				} else {
-					console.log(color.red("[ERROR]", color.magenta("<JSON Exception>"), color.yellow(error)));
+					console.log(color.red("[ERROR]", color.magenta("<Global Exception>"), color.yellow(error)));
 				}
 			}
-			try {
+		});
 
-				console.log(color.magenta("[DocBot]"), color.magenta(`<${ws.sessionID}>`), color.green("Received Crawl Request for Service"), color.red(messageJSON.service));
-
-
-				const options = {
-					hostname: 'edit.tosdr.org',
-					port: 443,
-					path: '/api/v1/services/' + messageJSON.service,
-					method: 'GET'
-				}
-				let str = "";
-				let matches = [];
-				console.log(color.magenta("[DocBot]"), color.magenta(`<${ws.sessionID}>`), color.green("Starting Request to Service"), color.magenta(messageJSON.service), color.red(crypto.createHash('md5').update(messageJSON.service).digest("hex")));
-
-				const req = https.request(options, response => {
-					response.on('data', function (chunk) {
-						if (parseInt(process.env.VERBOSITY) >= 3) {
-							console.log(color.magenta("[DocBot]"), color.magenta(`<${ws.sessionID}>`), color.green("Received Phoenix Chunk"), color.red(crypto.createHash('md5').update(chunk).digest("hex")));
-						}
-						str += chunk;
-					});
-
-					//the whole response has been received, so we just print it out here
-					response.on('end', () => {
-						console.log(color.magenta("[DocBot]"), color.magenta(`<${ws.sessionID}>`), color.cyan("Digested Phoenix Chunk(s)"), color.red(crypto.createHash('md5').update(str).digest("hex")));
-						let parsed = JSON.parse(str);
-						for (var documentIndex in parsed.documents) {
-							console.log(color.magenta("[DocBot]"), color.magenta(`<${ws.sessionID}>`), color.green("Parsed Document"), color.green(parsed.documents[documentIndex].name), color.red(crypto.createHash('md5').update(parsed.documents[documentIndex].text).digest("hex")));
-
-							if (parsed.documents[documentIndex].text === null) {
-								console.log(color.magenta("[DocBot]"), color.magenta(`<${ws.sessionID}>`), color.yellow("Skipping document, it's empty!"), color.red(crypto.createHash('md5').update(parsed.documents[documentIndex].text).digest("hex")));
-								continue;
-							}
-							let Sentences = parsed.documents[documentIndex].text.split(".\n");
-							console.log(color.magenta("[DocBot]"), color.magenta(`<${ws.sessionID}>`), color.cyan("Parsing Sentences"), color.red(crypto.createHash('md5').update(parsed.documents[documentIndex].text).digest("hex")));
-
-
-							regexs.forEach((RegularExpression) => {
-								for (var index in Sentences) {
-									if (RegularExpression.expression.test(Sentences[index]) && (!matches.includes(RegularExpression.caseID) && process.env.NO_DUPLICATES == "1")) {
-
-										let quoteStart = parsed.documents[documentIndex].text.indexOf(Sentences[index]);
-										let quoteEnd = quoteStart + Sentences[index].length;
-
-										console.log(color.magenta("[DocBot]"), color.magenta(`<${ws.sessionID}>`), color.green("I have found a match on Line"), color.magenta(index), color.green("for the case"), color.magenta(RegularExpression.caseID), color.red(crypto.createHash('md5').update(Sentences[index]).digest("hex")));
-
-
-										ws.send(Response.match(striptags(Sentences[index]).replace(/\n/g, ''), RegularExpression.caseID, parsed.documents[documentIndex].id, quoteStart, quoteEnd, ws.ContentType))
-										matches.push(RegularExpression.caseID);
-									}
-								}
-							});
-						}
-					});
-				})
-
-				req.on('error', error => {
-					console.error(error)
-				})
-
-				req.end();
-			} catch (error) {
-				ws.send(Response.error("error_exec", 0, ws.ContentType));
-				console.log(color.red("[ERROR]", color.magenta(`<${messageJSON.service}>`), color.yellow(error)));
-			}
-		} catch (error) {
-			ws.send(Response.error("error_exec", 0, ws.ContentType));
-			if (parseInt(process.env.VERBOSITY) >= 3) {
-				console.log(color.red("[ERROR]", color.magenta("<Global Exception Stacktrace>"), color.yellow(error.stack)));
-			} else {
-				console.log(color.red("[ERROR]", color.magenta("<Global Exception>"), color.yellow(error)));
-			}
-		}
 	});
-
-});
+})
