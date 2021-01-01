ca564aa13dc0bbe96795d2c89e5e430d11b3b1be
Justin René Back <justin@tosdr.org>
2021-01-01 23:30:57 +0100

    Update 118.ts

--- regex/118.ts
+++ regex/118.ts
@@ -2,6 +2,6 @@ import { Regex } from '../models';
 
 module.exports = {
 	expression: new RegExp("^((?=.*moral))((?=.*waive))", "i"),
-	case ID: 118,
+	caseID: 118,
 	name: "You waive your moral rights"
 } as Regex;
