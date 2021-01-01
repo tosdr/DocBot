e35f93338c92e93a882747c16195414ee8c8407b
Justin René Back <justin@tosdr.org>
2021-01-01 23:31:32 +0100

    Update 149.ts

--- regex/149.ts
+++ regex/149.ts
@@ -2,6 +2,6 @@ import { Regex } from '../models';
 
 module.exports = {
 	expression: new RegExp("^((?=.*liability))((?=.*exceed)|(?=.*paid by you))|(?=.*paid by the user))", "i"),
-	case ID: 149,
+	caseID: 149,
 	name: "Any liability on behalf of the service is only limited to the fees you paid as a user"
 } as Regex;
