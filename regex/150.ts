861f8a92e727d90a8ee3185a3d825462a0351133
Justin René Back <jback@pixelcatproductions.net>
2021-01-03 08:22:15 +0100

    Fix regex

--- regex/150.ts
+++ regex/150.ts
@@ -1,7 +1,7 @@
 import { Regex } from '../models';
 
 module.exports = {
-	expression: new RegExp("^((?=.*spidering)|(?=.*spider)|(?=.*crawler)|(?=.*crawling))((?=.*not)|(?=.*no))|((?=.*not)|(?=.*no))((?=.*automatic)|(?=.*automation)|(?=.*automate)(?=.*access))", "i"),
+	expression: new RegExp("^((?=.*spidering)|(?=.*spider)|(?=.*crawler)|(?=.*crawling))((?=.*not)|(?=.*no))|((?=.*not)|(?=.*no))((?=.*automatic )|(?=.*automation)|(?=.*automate)(?=.*access))", "i"),
 	expressionDont: new RegExp("", "i"),
 	caseID: 150,
 	name: "Spidering or crawling is not allowed"
--- regex/295.ts
+++ regex/295.ts
@@ -1,7 +1,7 @@
 import { Regex } from '../models';
 
 module.exports = {
-	expression: new RegExp("^((?=.*fail)|(?=.*enforce))((?=.*waiver))", "i"),
+	expression: new RegExp("^(?=.*failure)((?=.*exercise)|(?=.*enforce))((?=.*waiver))", "i"),
 	expressionDont: new RegExp("", "i"),
 	caseID: 295,
 	name: "Failure to enforce any provision of the Terms of Service does not constitute a waiver of such provision"
