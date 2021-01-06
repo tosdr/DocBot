442b4431b3dec79604df52b57cc4881c2715eb1e
Peepo1 <58347559+Peepo1@users.noreply.github.com>
2021-01-06 11:00:59 -0600

    2 Edits

--- regex/331.ts
+++ regex/331.ts
@@ -1,7 +1,7 @@
 import { Regex } from '../models';
 
 module.exports = {
-	expression: new RegExp("^((?=.*date\:)(?=.*effective))|((?=.*last modified)|(?=.*updated\:))|((?=.*update\:)|(?=.*last))|((?=.*Date of Last Revision)|(?=.*page was last edited)|(?=.*Last updated)|(?=.*Last Revised)(?=.*Version Date))", "i"),
+	expression: new RegExp("^(((?=.*date\:)(?=.*effective))|((?=.*Last modified\:)|(?=.*Last modified on)|(?=.*Date of Last Revision)|(?=.*page was last edited)|(?=.*Last updated)|(?=.*Last update\:)|(?=.*Last Revised)|(?=.*Version Date)))", "i"),
 	expressionDont: new RegExp("", "i"),
 	caseID: 331,
 	name: "There is a date of the last update of the terms"
--- regex/387.ts
+++ regex/387.ts
@@ -1,7 +1,7 @@
 import { Regex } from '../models';
 
 module.exports = {
-    expression: new RegExp("^((?=.*refer))((?=.*page)|(?=.*site))", "i"),
+    expression: new RegExp("^((?=.*visited )((?=.*prior)|(?=.*before))|(?=.*visited from)(?=.*domain)|((?=.*referrer)|(?=.*referral)|(?=.*referring)|(?=.*referred))((?=.*url)|(?=.*http)|(?=.*website)|(?=.*web page)|(?=.*address)))", "i"),
 	expressionDont: new RegExp("", "i"),
 	caseID: 387,
 	name: "This service tracks which web page referred you to it"
