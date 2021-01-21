f268aaac9771ce113367d939e6f818346b308153
Peepo1 <58347559+Peepo1@users.noreply.github.com>
2021-01-20 20:32:59 -0600

    2 add 1 modify

--- regex/152.ts
+++ regex/152.ts
@@ -1,7 +1,7 @@
 import { Regex } from '../models';
 
 module.exports = {
-	expression: new RegExp("^((?=.*at least)((?=.*years old)|(?=.*years of age))|(?=.*you must be)(?=.*old)|((?=.*years of age)|(?=.*under the age)|(?=.*age is under)|(?=.*not intended for)|(?=.*only for))((?=.*13)|(?=.*thirteen)|(?=.*16)|(?=.*sixteen)|(?=.*18)|(?=.*eighteen)))", "mi"),
+	expression: new RegExp("^((?=.*at least)((?=.*years old)|(?=.*years of age))|(?=.*you must be)(?=.*old)|((?=.*years of age)|(?=.*under the age)|(?=.*age is under)|(?=.*not intended for)|(?=.*only for))((?=.*13)|(?=.*thirteen)|(?=.*16)|(?=.*sixteen)|(?=.*18)|(?=.*eighteen))((?!.*California)))", "mi"),
 	expressionDont: new RegExp("", "i"),
 	caseID: 152,
 	name: "This service is only available to users over a certain age"
